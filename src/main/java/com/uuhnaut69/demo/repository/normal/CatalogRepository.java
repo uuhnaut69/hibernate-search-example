42b1f97d17d3c45ef233e65f3568ce831f9c3305
uuhnaut69 <uuhnaut96@gmail.com>
2020-02-19 09:57:32 +0700

    # Remove unused methods

--- src/main/java/com/uuhnaut69/demo/repository/normal/CatalogRepository.java
+++ src/main/java/com/uuhnaut69/demo/repository/normal/CatalogRepository.java
@@ -4,12 +4,9 @@ import com.uuhnaut69.demo.domain.model.Catalog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CatalogRepository extends JpaRepository<Catalog, UUID> {
-
-    List<Catalog> findByIdIn(List<UUID> uuids);
 
 }
